dbb400f097d4b180528a7c6182c6eb7daa579c8a
Joshua-Wu <joshuawu357@gmail.com>
2024-02-19 22:25:32 -0500

    initial commits

---
+++ app/src/main/java/com/example/myapplication/WelcomeScreen.java
@@ -0,0 +1,20 @@
+package com.example.myapplication;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class WelcomeScreen extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.welcome_screen);
+    }
+}
