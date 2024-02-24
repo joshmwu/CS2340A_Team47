125a285842af5abe9d9c4483d65a065be39b26b6
stevenliii <stevenli45678@gmail.com>
2024-02-24 16:23:44 -0500

    added login button functionality along with checks for whitespace and no input

--- app/src/main/java/com/example/myapplication/models/LoginData.java
+++ app/src/main/java/com/example/myapplication/models/LoginData.java
@@ -5,4 +5,16 @@ public class LoginData {
     private String username;
     private String password;
 
+    public String getUsername() {
+        return username;
+    }
+    public String getPassword() {
+        return password;
+    }
+    public void setUsername(String username) {
+        this.username = username;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
--- app/src/main/java/com/example/myapplication/views/LoginScreen.java
+++ app/src/main/java/com/example/myapplication/views/LoginScreen.java
@@ -9,6 +9,7 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -16,9 +17,19 @@ import com.example.myapplication.R;
 import com.example.myapplication.viewmodels.LoginScreenViewModel;
 
 public class LoginScreen extends AppCompatActivity {
+    private LoginScreenViewModel loginVM;
+    private EditText usernameET;
+    private EditText passwordET;
+    private TextView warningMessage;
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_screen);
+        loginVM = LoginScreenViewModel.getInstance();
+        usernameET = findViewById(R.id.usernameEditText);
+        passwordET = findViewById(R.id.passwordEditText);
+        warningMessage = findViewById(R.id.loginMessage);
+
 
         Button loginQuitButton = findViewById(R.id.loginExitButton);
         loginQuitButton.setOnClickListener(v -> {
@@ -27,5 +38,22 @@ public class LoginScreen extends AppCompatActivity {
             goHome.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
             startActivity(goHome);
         });
+
+        Button loginButton = findViewById(R.id.loginButton);
+        loginButton.setOnClickListener(v -> {
+            if (loginVM.checkNoInput(passwordET.getText()) || loginVM.checkNoInput(passwordET.getText())){
+                warningMessage.setText("Please enter a valid username and password");
+            } else if (loginVM.checkWhitespace(usernameET.getText()) || loginVM.checkWhitespace(passwordET.getText())) {
+                warningMessage.setText("Please check to ensure you have no whitespace");
+            } else if (false/*invalid login, needs firebase functionality*/){
+                warningMessage.setText("Invalid credentials, please try again");
+            } else {
+                loginVM.updateLoginData(usernameET.getText().toString(), passwordET.getText().toString());
+                //change this to homescreen once its made
+                Intent toAppHomeScreen = new Intent(LoginScreen.this, WelcomeScreen.class);
+                startActivity(toAppHomeScreen);
+            }
+        });
+
     }
 }
