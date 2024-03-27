2d0eb45496a3cea0ae14e78892d7dde80ea891c4
stevenliii <stevenli45678@gmail.com>
2024-03-27 13:22:05 -0400

    added ingredient screen form

--- app/src/main/java/com/example/myapplication/views/AddIngredientsScreenFrag.java
+++ app/src/main/java/com/example/myapplication/views/AddIngredientsScreenFrag.java
@@ -13,25 +13,25 @@ import android.view.ViewGroup;
 import com.example.myapplication.R;
 import android.widget.Button;
 
-public class IngredientsScreenFrag extends Fragment {
-    private Button addToPantryButton;
+public class AddIngredientsScreenFrag extends Fragment {
+    private Button addIngredient;
+    private Button backButton;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View root = inflater.inflate(R.layout.fragment_ingredients_screen, container, false);
+        View root = inflater.inflate(R.layout.fragment_add_to_pantry, container, false);
+        addIngredient = root.findViewById(R.id.addIngredientButton);
 
-        addToPantryButton = root.findViewById(R.id.addToPantryButton);
-        addToPantryButton.setOnClickListener(v -> {
-            replaceFragment(new AddIngredientsScreenFrag());
+        backButton = root.findViewById(R.id.addIngredientsBackButton);
+        backButton.setOnClickListener(v -> {
+            replaceFragment(new IngredientsScreenFrag());
         });
-
 
 
 
         return root;
     }
-
     private void replaceFragment(Fragment fragment) {
         FragmentManager fragmentManager = getParentFragmentManager();
         FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
