807a147fdd2d361c805aec530c1cc5b50c2ddcec
lilyrren <lilyyren@gmail.com>
2024-04-16 00:12:16 -0400

    one more junit

--- app/src/main/java/com/example/myapplication/views/RecipeDetailsFrag.java
+++ app/src/main/java/com/example/myapplication/views/RecipeDetailsFrag.java
@@ -171,7 +171,7 @@ public class RecipeDetailsFrag extends Fragment {
         return name;
     }
 
-    private static int getItemQuantity(String item) {
+    public static int getItemQuantity(String item) {
         String quantity = "";
         for (int i = item.length() - 1; i < item.length() && !item.substring(i, i+1).equals(" "); i--) {
             quantity = item.substring(i, i+1) + quantity;
@@ -187,5 +187,4 @@ public class RecipeDetailsFrag extends Fragment {
         // Add other entries as needed
         return updatedPieEntries;
     }
-
 }
